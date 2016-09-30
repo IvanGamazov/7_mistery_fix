d9a6038c583446335b63ef86b0119c0a2ac9cbe3
IvanGamazov <ivan.gamazov@gmail.com>
2016-09-30 22:25:01 +0300

    Исправил ошибки - убрал лишние файлы, написал ридми.

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,4 +1,5 @@
 from math import sqrt
+from pprint import pprint
 
 
 def get_roots(a, b, c):
@@ -12,3 +13,39 @@ def get_roots(a, b, c):
             return root1, root2
     else:
         return None, None
+
+
+def check_coefficients(a ,b ,c):
+    if not a:
+        a = 0
+    else:
+        a = int(a)
+    if not b:
+        b = 0
+    else:
+        b = int(b)
+    if not c:
+        c = 0
+    else:
+        c = int(c)
+    return a, b, c
+
+
+def print_result(root1, root2):
+    pprint("Solution: first root : {0}, second root: {1}".format(root1, root2))
+
+
+def main():
+    a = input("Enter the a-coefficient for x^2 --> ")
+    b = input("Enter the b-coefficient for x --> ")
+    c = input("Enter the c-coefficient - a free member of the equation --> ")
+
+    a, b, c = check_coefficients(a, b, c)
+    root1, root2 = get_roots(a, b, c)
+    print_result(root1, root2)
+
+
+if __name__ == "__main__":
+    main()
+
+
